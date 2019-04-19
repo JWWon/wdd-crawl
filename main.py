772df0d3a1941aa6c5fd9d578155efd190f81cbc
JWWon <wonjiwn@naver.com>
2019-04-19 19:32:25 +0900

    Update seraching keyword -> add location

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ sheet_name = 'Data'
 
 keywords = pd.read_csv('keyword.csv')
 writer = pd.ExcelWriter(filename, engine='openpyxl')
+locations = ['서울', '인천', '경기', '충청', '대전', '대구', '광주', '전라도', '제주', '강원도']
 
 # DELETE FILE
 try:
@@ -31,33 +32,35 @@ for _, row in keywords.iterrows():
     keyword = row['keyword']
     print(f'## KEYWORD: {keyword}')
 
-    for i in range(34):
-        params = {"query": keyword, "display": 30, "start": i * 30 + 1}
-        res = requests.get(baseURL, params=params, headers=headers)
+    for location in locations:
+        for i in range(34):
+            params = {"query": f'{location} {keyword}',
+                      "display": 30, "start": i * 30 + 1}
+            res = requests.get(baseURL, params=params, headers=headers)
 
-        if res.status_code == 200:
-            data = res.json()
-            items = data['items']
-            items_len = len(items)
-            print(f'  count: {i}, results: {items_len}')
+            if res.status_code == 200:
+                data = res.json()
+                items = data['items']
+                items_len = len(items)
+                print(f'  count: {i}, results: {items_len}')
 
-            for item in items:
-                titles = re.sub('(<b>|</b>)', ' ', item['title'])
-                titles.strip()
-                del item['title']
-                item['title'] = titles
+                for item in items:
+                    titles = re.sub('(<b>|</b>)', ' ', item['title'])
+                    titles.strip()
+                    del item['title']
+                    item['title'] = titles
 
-                if item['description']:
-                    descriptions = re.sub(
-                        '(<b>|</b>)', ' ', item['description'])
-                    descriptions.strip()
-                    del item['description']
-                    item['description'] = descriptions
+                    if item['description']:
+                        descriptions = re.sub(
+                            '(<b>|</b>)', ' ', item['description'])
+                        descriptions.strip()
+                        del item['description']
+                        item['description'] = descriptions
 
-            # convert items to dataframe
-            df = pd.DataFrame(items)
-            append_df_to_excel(
-                filename, df, sheet_name=sheet_name, index=False)
+                # convert items to dataframe
+                df = pd.DataFrame(items)
+                append_df_to_excel(
+                    filename, df, sheet_name=sheet_name, index=False)
 
-            if items_len < 30:
-                break
+                if items_len < 30:
+                    break
